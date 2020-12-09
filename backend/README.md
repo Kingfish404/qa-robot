6fe34982fa07d4b6a3d2f8c035e73aca02d42fb8
Kingfish404 <jinyu929@qq.com>
2020-12-09 10:23:51 +0800

    init

---
+++ backend/README.md
@@ -0,0 +1,27 @@
+# backend
+
+QARobot后端,主要的功能函数写在./robot/view.py里面
+
+## 项目结构
+
+* backend 文件夹-项目设置文件夹  
+* robot 文件夹-项目主程序文件夹
+
+URL设置，在`./robot/urls.py`中配置URL及其方法
+
+## 项目配置
+
+项目启动前，需要对数据库进行初始化，命令如下
+
+```shell
+python3 manage.py migrate
+```
+
+启动项目的命令
+```shell
+python3 manage.py runserver
+```
+
+## 依赖
+
+* django
---
+++ backend/robot/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+
+def index(request):
+    return render(request,"index.html")
+
+@csrf_exempt
+def msgAsk(request):
+    # 文字问答的处理程序
+    json = {"code": 0, "data": {"answer": ""}, "msg": ""}
+    answer = None
+    if(request.method == 'GET'):
+        json['code'] = 400
+        json['msg'] = "ERROR:请求方式错误"
+
+    elif(request.method == 'POST'):
+        json['code'] = 200
+        json['msg'] = "SUCCESS"
+        
+        try:
+            # 主要的语言处理函数
+            question = username = request.POST.get('question')
+            if question == None:
+                question = "No msg"
+
+            
+
+            # answer 为回复
+            answer = "Hello,q="+question
+        except Exception as ex:
+            question = ex.message
+        
+        json["data"]['answer'] = answer
+    else:
+        json['code'] = 404
+        json['msg'] = "FAILD:未执行功能"
+
+    return JsonResponse(json)
